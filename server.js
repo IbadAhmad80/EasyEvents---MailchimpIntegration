0e840a45f8dcf8c18a6c5e7df492cc7bc945704f
Ibad Ahmad <ibad23ahmad@gmail.com>
2024-02-04 17:08:32 +0500

    Update linting

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
 
 const MAILCHIMP_CLIENT_ID = process.env.MAILCHIMP_CLIENT_ID;
@@ -23,122 +23,98 @@ const MAILCHIMP_CLIENT_SECRET = process.env.MAILCHIMP_CLIENT_SECRET;
 const OAUTH_CALLBACK = process.env.OAUTH_CALLBACK;
 
 app.get("/oauth/mailchimp/callback", async (req, res) => {
-  const {
-    query: { code },
-  } = req;
+    const {
+        query: { code },
+    } = req;
 
-  const tokenResponse = await fetch(
-    "https://login.mailchimp.com/oauth2/token",
-    {
-      method: "POST",
-      body: new URLSearchParams({
-        grant_type: "authorization_code",
-        client_id: MAILCHIMP_CLIENT_ID,
-        client_secret: MAILCHIMP_CLIENT_SECRET,
-        redirect_uri: OAUTH_CALLBACK,
-        code: code,
-      }),
-    }
-  );
+    const tokenResponse = await fetch("https://login.mailchimp.com/oauth2/token", {
+        method: "POST",
+        body: new URLSearchParams({
+            grant_type: "authorization_code",
+            client_id: MAILCHIMP_CLIENT_ID,
+            client_secret: MAILCHIMP_CLIENT_SECRET,
+            redirect_uri: OAUTH_CALLBACK,
+            code: code,
+        }),
+    });
 
-  const { access_token } = await tokenResponse.json();
+    const { access_token } = await tokenResponse.json();
 
-  const metadataResponse = await fetch(
-    "https://login.mailchimp.com/oauth2/metadata",
-    {
-      headers: {
-        Authorization: `OAuth ${access_token}`,
-      },
-    }
-  );
+    const metadataResponse = await fetch("https://login.mailchimp.com/oauth2/metadata", {
+        headers: {
+            Authorization: `OAuth ${access_token}`,
+        },
+    });
 
-  const { dc } = await metadataResponse.json();
+    const { dc } = await metadataResponse.json();
 
-  res.status(200).send({ access_token, dc });
+    res.status(200).send({ access_token, dc });
 });
 
 app.get("/audiences", async (req, res) => {
-  try {
-    const {
-      query: { accessToken, server },
-    } = req;
+    try {
+        const {
+            query: { accessToken, server },
+        } = req;
 
-    console.log("accessToken", accessToken, server);
+        console.log("accessToken", accessToken, server);
 
-    mailchimp.setConfig({
-      accessToken,
-      server,
-    });
+        mailchimp.setConfig({
+            accessToken,
+            server,
+        });
 
-    let audiences = [];
-    const listsResponse = await mailchimp.lists.getAllLists();
+        let audiences = [];
+        const listsResponse = await mailchimp.lists.getAllLists();
 
-    for (const list of listsResponse.lists) {
-      const contacts = await mailchimp.lists.getListMembersInfo(list.id, {
-        status: "subscribed",
-        count: 300,
-        offset: 0,
-      });
+        for (const list of listsResponse.lists) {
+            const contacts = await mailchimp.lists.getListMembersInfo(list.id, {
+                status: "subscribed",
+                count: 300,
+                offset: 0,
+            });
 
-      audiences.push({
-        email_addresses: contacts.members.map(
-          (contact) => contact.email_address
-        ),
-        list_name: list.name,
-        list_id: list.id,
-      });
+            audiences.push({
+                email_addresses: contacts.members.map((contact) => contact.email_address),
+                list_name: list.name,
+                list_id: list.id,
+            });
+        }
+
+        res.status(200).send({ audiences });
+    } catch (error) {
+        res.status(500).send(`Error fetching contacts from lists. ${error}`);
     }
-
-    res.status(200).send({ audiences });
-  } catch (error) {
-    res.status(500).send(`Error fetching contacts from lists. ${error}`);
-  }
 });
 
 app.get("/campaigns", async (req, res) => {
-  try {
-    const {
-      emails,
-      scheduleTime,
-      listId,
-      subject,
-      body,
-      access_token,
-      server,
-    } = req.query;
+    try {
+        const { emails, scheduleTime, listId, subject, body, access_token, server } = req.query;
 
-    mailchimp.setConfig({
-      apiKey: access_token,
-      server,
-    });
+        mailchimp.setConfig({
+            apiKey: access_token,
+            server,
+        });
 
-    console.log("contacts", emails, scheduleTime, listId, subject, body);
+        console.log("contacts", emails, scheduleTime, listId, subject, body);
 
-    for (const id of JSON.parse(listId)) {
-      const campaignBody = composeCampaignBody(
-        emails,
-        scheduleTime,
-        id,
-        subject,
-        body
-      );
+        for (const id of JSON.parse(listId)) {
+            const campaignBody = composeCampaignBody(emails, scheduleTime, id, subject, body);
 
-      const response = await mailchimp.campaigns.create(campaignBody);
+            const response = await mailchimp.campaigns.create(campaignBody);
+        }
+
+        // await mailchimp.campaigns.schedule(response.id, {
+        //   schedule_time: "2024-03-23T19:38:46.300Z",
+        // });
+
+        res.status(200).send({ message: `Campaign created successfully at ${scheduleTime}` });
+    } catch (error) {
+        console.log("error", error);
+        res.status(500).send(`Error creating campaign. ${error}`);
     }
-
-    // await mailchimp.campaigns.schedule(response.id, {
-    //   schedule_time: "2024-03-23T19:38:46.300Z",
-    // });
-
-    res
-      .status(200)
-      .send({ message: `Campaign created successfully at ${scheduleTime}` });
-  } catch (error) {
-    console.log("error", error);
-    res.status(500).send(`Error creating campaign. ${error}`);
-  }
 });
 
 app.listen(3000, "127.0.0.1", function () {
-  console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
 });
